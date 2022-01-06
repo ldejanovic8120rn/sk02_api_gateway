95e5bb1c70619a580bd527b62ca8392fd494e902
Lazar Dejanovic <lazzar99@gmail.com>
2022-01-06 17:16:58 +0100

    Initial commit

---
+++ src/main/java/com/sk02/sk02_api_gateway/Sk02ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sk02.sk02_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sk02ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sk02ApiGatewayApplication.class, args);
+    }
+
+}
